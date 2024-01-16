2b01421bf64189736a1159aa97c463189122ea06
HealKnix <www.daniiland@gmail.com>
2024-01-17 00:43:00 +0800

    extension activation after installation

--- extension.ts
+++ extension.ts
@@ -3,38 +3,28 @@ import ActiveSettings from './settings/active'
 import DisableSettings from './settings/disable'
 
 export function activate(context: vscode.ExtensionContext) {
-  console.log('Congratulations, your extension "MacOS titlebar for Windows" is now active!')
+  ;async () => {
+    const myExtension = vscode.extensions.getExtension('healknix.macos-titlebar-for-windows')
+    const currentVersion = myExtension!.packageJSON.version ?? '0.0.0'
+
+    const lastVersion = context.globalState.get('MyExtensionVersion')
+    if (currentVersion !== lastVersion) {
+      void context.globalState.update('MyExtensionVersion', currentVersion)
+      await setupExtention()
+    }
+  }
 
   let disposableActive = vscode.commands.registerCommand(
     'macos-titlebar-for-windows.active',
     async () => {
-      // vscode.window.showInformationMessage('Mac Style Controls is now Active!')
-      let userSettings = vscode.workspace.getConfiguration()
-      const currentSettings = userSettings.get('apc.stylesheet', {})
-
-      await userSettings.update('msc.active', true, true)
-      await userSettings.update('window.menuBarVisibility', 'compact', true)
-      await userSettings.update(
-        'apc.electron',
-        {
-          titleBarStyle: 'hiddenInset'
-        },
-        true
-      )
-
-      const updatedSettings = {
-        ...currentSettings,
-        ...ActiveSettings
-      }
-
-      await userSettings.update('apc.stylesheet', updatedSettings, true)
+      await setupExtention()
     }
   )
 
   let disposableDisable = vscode.commands.registerCommand(
     'macos-titlebar-for-windows.disable',
     async () => {
-      // vscode.window.showInformationMessage('Mac Style Controls is now Disabled!')
+      vscode.window.showInformationMessage('MacOS Titlebar is now Disabled!')
       let userSettings = vscode.workspace.getConfiguration()
       const currentSettings = userSettings.get('apc.stylesheet', {})
 
@@ -55,16 +45,41 @@ export function activate(context: vscode.ExtensionContext) {
   context.subscriptions.push(disposableActive, disposableDisable)
 }
 
-export async function deactivate() {
+export function deactivate() {
+  ;async () => {
+    let userSettings = vscode.workspace.getConfiguration()
+    const currentSettings = userSettings.get('apc.stylesheet', {})
+
+    await userSettings.update('msc.active', false, true)
+    await userSettings.update('window.menuBarVisibility', 'classic', true)
+
+    const updatedSettings = {
+      ...currentSettings,
+      ...DisableSettings
+    }
+
+    await userSettings.update('apc.stylesheet', updatedSettings, true)
+  }
+}
+
+async function setupExtention() {
+  vscode.window.showInformationMessage('MacOS Titlebar is now Active!')
   let userSettings = vscode.workspace.getConfiguration()
   const currentSettings = userSettings.get('apc.stylesheet', {})
 
-  await userSettings.update('msc.active', false, true)
-  await userSettings.update('window.menuBarVisibility', 'classic', true)
+  await userSettings.update('msc.active', true, true)
+  await userSettings.update('window.menuBarVisibility', 'compact', true)
+  await userSettings.update(
+    'apc.electron',
+    {
+      titleBarStyle: 'hiddenInset'
+    },
+    true
+  )
 
   const updatedSettings = {
     ...currentSettings,
-    ...DisableSettings
+    ...ActiveSettings
   }
 
   await userSettings.update('apc.stylesheet', updatedSettings, true)
